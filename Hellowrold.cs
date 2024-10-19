738fe4f0583e02f8dbd467ce66442eb3c79415cf
madgees666newme579 <madgeessalahs@gmail.com>
2024-10-19 16:56:14 +0200

    Add files via upload

---
+++ Hellowrold.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hellowrold : MonoBehaviour
+{
+    // Start is roblox 99 new update
+    void Start()
+    {
+        // meis good with roblox join NOW
+        print("hello world!");
+    }
+
+    // roblox is 100th update
+    void Update()
+    {
+        
+    }
+}
